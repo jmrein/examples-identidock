9e5af874681b5129c076e225cf25a5ca9db65b55
jmrein <jr27@sanger.ac.uk>
2018-03-27 14:35:50 +0000

    Fix problem revealed by unit tests
    And cleanup the startup scripts

--- app/tests.py
+++ app/tests.py
@@ -14,7 +14,7 @@ class TestCase(unittest.TestCase):
 
     def test_html_escaping(self):
         page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
-        asssert '<b>' not in str(page.data)
+        assert '<b>' not in str(page.data)
 
 if __name__ == '__main__':
     unittest.main()
--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+echo "cmd.sh: $ENV"
 if [ "$ENV" = 'DEV' ]; then
-  echo "Running Development Server"
-  exec python "identidock.py"
+    echo "Running Development Server"
+    exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running unit tests"
+    exec python "tests.py"
 else
-  echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+    echo "Running Production Server"
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
              --callable app --stats 0.0.0.0:9191
 fi
